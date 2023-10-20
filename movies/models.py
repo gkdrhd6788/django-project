f60cfec8ccc2a07ff6463361b1dd7fd8f55ee641
Sooji Kwon <gkdrhd6788@gmail.com>
2023-10-20 17:19:10 +0900

    delete completed

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
+    content = models.TextField()
+    
--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+
 app_name = 'movies'
-
-
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('create/',views.create,name='create'),
+    path('<int:movie_pk>/',views.detail,name='detail'),
+    path('<int:movie_pk>/update/',views.update,name='update'),
+    path('<int:movie_pk>/delete/',views.delete,name='delete'),
 ]
